910833516e61754184011ce5f81533d5154e09a2
Shivashis Padhi <shivashispadhi@gmail.com>
2024-12-15 17:33:04 +0100

    Improve CLI and respect arguments/flags

--- internal/baler/convert.go
+++ internal/baler/convert.go
@@ -3,9 +3,10 @@ package baler
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"path"
 	"path/filepath"
-	"slices"
 	"strconv"
 	"unicode/utf8"
 )
@@ -13,40 +14,119 @@ import (
 // TODO: figure out how to store and update this information alt. global variable
 var fileCounter = 0
 
-func checkValidUTF8(fileName string) (bool, error) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		return false, err
-	}
-	defer file.Close()
+type ConvertConfig struct {
+	MaxInputFileLines uint64
+	MaxInputFileSize  uint64
+	MaxOutputFileSize uint64
+	ExclusionPatterns *[]string
+}
+
+type ValidationResult struct {
+	IsValidUTF8  bool
+	IsValidLines bool
+	IsValidSize  bool
+	// artifacts
+	Size uint64
+}
+
+func customScanner(file *os.File) *bufio.Scanner {
 	scanner := bufio.NewScanner(file)
 	buf := make([]byte, 0, 64*1024)
 	scanner.Buffer(buf, 6*1024*1024)
-	var content []byte
-	for lineCount := 0; lineCount < 10 && scanner.Scan(); lineCount++ {
-		content = append(content, scanner.Bytes()...)
-		content = append(content, '\n')
-	}
-
-	if err := scanner.Err(); err != nil {
-		return false, fmt.Errorf("error scanning file: %v", err)
-	}
-	return utf8.Valid(content), nil
+	return scanner
 }
 
-func convertDirectoryAndSaveToFile(sourcePath string, destinationDir string) error {
+func validateFile(fileName string, config *ConvertConfig) (*ValidationResult, error) {
+	// TODO: check if it's feasible to return early
+	isValidUTF8 := true
+	isValidLines := true
+	isValidSize := true
+	// checks without opening the file
+	fileInfo, err := os.Stat(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get file info: %w", err)
+	}
+
+	if fileInfo.Size() > int64(config.MaxInputFileSize) {
+		isValidSize = false
+	}
+	// checks including reads of the file
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	scanner := customScanner(file)
+	lineCount := uint32(0)
+
+	for scanner.Scan() {
+		lineCount++
+		if lineCount <= 10 && !utf8.Valid(scanner.Bytes()) {
+			isValidUTF8 = false
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error scanning file: %v", err)
+	}
+	if lineCount > uint32(config.MaxInputFileLines) {
+		isValidLines = false
+	}
+	return &ValidationResult{
+		IsValidUTF8:  isValidUTF8,
+		IsValidLines: isValidLines,
+		IsValidSize:  isValidSize,
+		Size:         uint64(fileInfo.Size()),
+	}, nil
+}
+
+func shouldIgnore(relativePath string, patternList *[]string) (bool, error) {
+	for _, pattern := range *patternList {
+		matches, err := path.Match(pattern, relativePath)
+		if err != nil {
+			return false, err
+		}
+		if matches {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func copyContent(srcPath string, destFile *os.File, srcRelativePath string) error {
+	srcFile, err := os.Open(srcPath)
+	if err != nil {
+		return fmt.Errorf("failed to open source file %w", err)
+	}
+	defer srcFile.Close()
+
+	reader := bufio.NewReader(srcFile)
+	writer := bufio.NewWriter(destFile)
+
+	if _, err := writer.WriteString(fmt.Sprintf("\n\n// filename: %s\n\n", srcRelativePath)); err != nil {
+		return fmt.Errorf("failed to write filename comment: %w", err)
+	}
+	if _, err = io.Copy(writer, reader); err != nil {
+		return fmt.Errorf("error copying file: %w", err)
+	}
+	if err := writer.Flush(); err != nil {
+		return fmt.Errorf("error flushing writer: %w", err)
+	}
+	return nil
+}
+func convertDirectoryAndSaveToFile(absProcessingDirPath string, sourcePath string, destinationDir string, config *ConvertConfig) error {
 	// sort entries alphabetically
-	entries, err := os.ReadDir(sourcePath)
+	entries, err := os.ReadDir(absProcessingDirPath)
 	if err != nil {
 		return err
 	}
-	// create new file in destinationPath
+	absSourcePath, err := filepath.Abs(sourcePath)
+	if err != nil {
+		return err
+	}
+	// reference to file in destinationPath
 	outputFileName := fmt.Sprintf("output_%s.txt", strconv.Itoa(fileCounter))
 	destinationFileName := filepath.Join(destinationDir, outputFileName)
 
-	// TODO: work with explicit mode (which doesn't assume things)
-	filesToIgnore := []string{".DS_Store", "package-lock.json"}
-	directoriesToIgnore := []string{"node_modules", ".git", ".expo"}
 	f, err := os.OpenFile(destinationFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
@@ -54,94 +134,93 @@ func convertDirectoryAndSaveToFile(sourcePath string, destinationDir string) err
 	// TODO: how would clean-up work in recursion
 	defer f.Close()
 
-	// absolute path preparation
-	absSourcePath, err := filepath.Abs(sourcePath)
-	if err != nil {
-		return err
-	}
-
-	// the loop
+	// iterate through entries
 	for _, entry := range entries {
-		absPath := filepath.Join(absSourcePath, entry.Name())
+		absPath := filepath.Join(absProcessingDirPath, entry.Name())
+		relPath, err := filepath.Rel(absSourcePath, absPath)
+		if err != nil {
+			return err
+		}
+
+		// ignore logic
+		if ignore, err := shouldIgnore(relPath, config.ExclusionPatterns); err != nil {
+			return err
+		} else if ignore {
+			continue
+		}
 
 		// for each file write the file into converted text file
 		// for each directory, run convertDirectoryAndSaveToFile recursively
 		if !entry.IsDir() {
-			// ignore logic
-			if slices.Contains(filesToIgnore, entry.Name()) {
-				continue
-			}
-
-			// check valid utf-8
-			validUtf, err := checkValidUTF8(absPath)
+			// file validation before processing
+			validationResult, err := validateFile(absPath, config)
 			if err != nil {
 				return err
 			}
-			if !validUtf {
-				// baler only supports text files
+			if !validationResult.IsValidLines || !validationResult.IsValidSize || !validationResult.IsValidUTF8 {
+				// TODO: log
 				continue
 			}
-			// read entry to data
-			data, err := os.ReadFile(absPath)
-			if err != nil {
-				return err
-			}
-			file_name_comment := []byte("\n// filename: " + entry.Name() + "\n\n")
-			data = append(file_name_comment, data...)
-			// before writing this data make sure it doesn't exceed the filesize.
+			// check if entry + existing sink file exceeds size limit
+			// if so, increment file name counter and set it as sink
 			currentDestinationFileInfo, err := f.Stat()
 			if err != nil {
 				return err
 			}
 			currentDestinationFileSize := currentDestinationFileInfo.Size()
-			// 5242880 = ~5MB
-			if currentDestinationFileSize+int64(len(data)) > int64(5242880) {
-				fileCounter += 1
+			// 5242880 = 5 * 1024 * 1024
+			if currentDestinationFileSize+int64(validationResult.Size) > int64(config.MaxOutputFileSize) {
+				// close reference to old file
 				f.Close()
-				// update reference to file
+
+				// update reference to new file
 				/*
 					TODO: Ideally the function call could be idempotent if 'READ' status
 					is maintained somewhere.
 					In which case, we could just increment fileCounter and call the
 					function again.
 				*/
+				fileCounter += 1
 				outputFileName = fmt.Sprintf("output_%s.txt", strconv.Itoa(fileCounter))
 				destinationFileName = filepath.Join(destinationDir, outputFileName)
 				f, err = os.OpenFile(destinationFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 				if err != nil {
 					return err
 				}
+				defer f.Close()
 			}
-			if _, err := f.Write(data); err != nil {
+			// perform copy
+			if err = copyContent(absPath, f, relPath); err != nil {
 				return err
 			}
+
 		} else {
-			if slices.Contains(directoriesToIgnore, entry.Name()) {
-				continue
-			}
 			// process directory
-			convertDirectoryAndSaveToFile(absPath, destinationDir)
+			// TODO: Could there theoretically be infinite open references to files?
+			convertDirectoryAndSaveToFile(absPath, sourcePath, destinationDir, config)
 		}
 	}
 	return nil
 }
 
-func Convert(path string, limit uint32) error {
-	pathFixed := "/Users/shiv/code/experimental/heli-exp"
-	// check if path exists
-	_, err := os.Stat(pathFixed)
-	if err != nil {
-		// TODO: create custom internal errors
+func Convert(inputPath string, outputPath string, config *ConvertConfig) error {
+	// check if input, output paths exists
+	if _, err := os.Stat(inputPath); err != nil {
 		return err
 	}
-	// create temporary directory
-	dname, err := os.MkdirTemp("", "outputdir")
-	if err != nil {
+	if _, err := os.Stat(outputPath); err != nil {
 		return err
 	}
-	err = convertDirectoryAndSaveToFile(pathFixed, dname)
+	absInputPath, err := filepath.Abs(inputPath)
 	if err != nil {
 		return err
 	}
+	if err := convertDirectoryAndSaveToFile(absInputPath, inputPath, outputPath, config); err != nil {
+		return err
+	}
+	return nil
+}
+
+func UnConvert(outputPath string, inputPath string) error {
 	return nil
 }
--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -29,16 +29,30 @@ func AddCommands() {
 	}
 
 	// convert a repository
+	var maxInputFileSize uint64
+	var maxInputFileLines uint64
+	var maxOutputFileSize uint64
+	var exclusionPatterns []string
 	var convertCmd = &cobra.Command{
 		Use:   "convert",
 		Short: "Convert a directory into the minimum number of text files with size < limit specified.",
 		Long: `Convert a directory into the minimum number of text files with size < limit specified.
 
-		Arguments: <path>
+		Arguments: <source-files-directory> <converted-files-directory>
+
+		e.g/
+
+		$ baler convert code_directory/ output_directory/
 		`,
-		Args: cobra.ExactArgs(1),
+		Args: cobra.ExactArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
-			err := baler.Convert(args[0], uint32(5120))
+			convertConfig := &baler.ConvertConfig{
+				MaxInputFileLines: maxInputFileLines,
+				MaxInputFileSize:  maxInputFileSize,
+				MaxOutputFileSize: maxOutputFileSize,
+				ExclusionPatterns: &exclusionPatterns,
+			}
+			err := baler.Convert(args[0], args[1], convertConfig)
 			if err != nil {
 				// TODO: remove this print
 				cmd.PrintErrln(err)
@@ -46,5 +60,32 @@ func AddCommands() {
 			}
 		},
 	}
-	BalerCommand.AddCommand(versionCmd, convertCmd)
+	convertCmd.Flags().Uint64VarP(&maxInputFileSize, "max-file-size", "u", 1*1024*1024, "Set maximum file size (in bytes) to be considered while converting.")
+	convertCmd.Flags().Uint64VarP(&maxInputFileLines, "max-file-lines", "l", 10000, "Set maximum lines a file can have to be considered while converting.")
+	convertCmd.Flags().Uint64VarP(&maxOutputFileSize, "max-output-file-size", "s", 5*1024*1024, "Set maximum size (in bytes) of the generated output file.")
+	convertCmd.Flags().StringSliceVarP(&exclusionPatterns, "exclusion-patterns", "e", []string{}, "A list of exclusion patterns for baler. e.g '-e \"node_modules*\" -e \"poetry.*\" -e \"package.*\"'")
+
+	// unconvert a group of files into directory
+	var unconvertCmd = &cobra.Command{
+		Use:   "unconvert",
+		Short: "From a group of converted text files, construct the initial directory of files.",
+		Long: `Reconstruct the group of text files used for 'baler convert', from the output files of baler.
+
+		Arguments: <converted-files-directory> <source-files-directory>
+
+		e.g/
+
+		$ baler unconvert output_directory/ new_code_directory/
+		`,
+		Args: cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			err := baler.UnConvert(args[0], args[1])
+			if err != nil {
+				// TODO: remove this print
+				cmd.PrintErrln(err)
+				cmd.Println("Unexpected error occured!")
+			}
+		},
+	}
+	BalerCommand.AddCommand(versionCmd, convertCmd, unconvertCmd)
 }
